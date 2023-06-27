a1a3c9a300b282392ddab83d628e25fe5957841e
iliaciuk <iliaciuk37@gmail.com>
2023-06-27 21:52:01 +0300

    Build React CRUD frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import router from './router.jsx'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/> 
+    <App />
   </React.StrictMode>
 );
 
